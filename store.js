b08397c88c74475e690e9c59146d08f7b7b7e859
ccruml20 <ccrumley1@gmail.com>
2017-07-20 16:33:51 -0600

    Encrypt passwords

--- store.js
+++ store.js
@@ -1,11 +1,12 @@
 const knex = require('knex')(require('./knexfile'))
+const crypto = require('crypto')
 
-// module.exports = {
-//   createUser ({ username, password }) {
-//     console.log(`Add user ${username} with password ${password}`)
-//     return Promise.resolve()
-//   }
-// }
+module.exports = {
+  createUser ({ username, password }) {
+    console.log(`Add user ${username} with password ${password}`)
+    return Promise.resolve()
+  }
+}
 
 
 module.exports = {
@@ -16,4 +17,32 @@ module.exports = {
       password
     })
   }
+}
+
+
+
+module.exports = {
+  saltHashPassword,
+  createUser ({ username, password }) {
+    console.log(`Add user ${username}`)
+    const { salt, hash } = saltHashPassword(password)
+    return knex('user').insert({
+      salt,
+      encrypted_password: hash,
+      username
+    })
+  }
+}
+function saltHashPassword (password) {
+  const salt = randomString()
+  const hash = crypto
+    .createHmac('sha512', salt)
+    .update(password)
+  return {
+    salt,
+    hash: hash.digest('hex')
+  }
+}
+function randomString () {
+  return crypto.randomBytes(4).toString('hex')
 }
